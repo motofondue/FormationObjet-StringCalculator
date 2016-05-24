4e2d952be113877cffe3bb1891c89df0c5fbf04f
motofondue <motofondue@no-log.org>
2016-05-24 09:36:25 +0200

    Empty string test

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,12 @@
 
 public class StringCalculator {
 
+	public int add(String numbers) {
+		if (numbers.equals("")) {
+			return 0;
+		}
+		else {
+			return -1;
+		}
+	}
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,4 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
-	
+	@Test
+	public void emptyStringToZero() {
+		// Given
+		StringCalculator sc = new StringCalculator();
+		// When
+		int sum = sc.add("");
+		// Then
+		assertEquals(0, sum);
+	}
 }
